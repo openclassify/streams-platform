4b411c96d540e9a97738bfea56df47154c623e11
Dia Shalabi <45572261+diashalabi@users.noreply.github.com>
2022-01-18 11:06:12 +0300

    #5159 openclassify new setup

--- resources/config/distribution.php
+++ resources/config/distribution.php
@@ -11,7 +11,7 @@ return [
     |
     */
 
-    'name'        => 'PHP Classifieds Marketplace software',
-    'description' => 'OpenClassify is modular and most advanced open source classified platform build with Laravel.Use it in real estate classifieds, auto, e-commerce and other ...',
+    'name'        => '',
+    'description' => '',
     'version'     => 'v1.1',
 ];
